0da27c9c6d97d805c857fb8173415ce802f936ff
MiguelMartinez <daw108.2022@gmail.com>
2022-02-23 09:22:18 +0100

    Añadida Resta

--- src/calculadoras/principal.java
+++ src/calculadoras/principal.java
@@ -4,7 +4,10 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		calculadora c1 = new calculadora();
+		calculadora miCalc = new calculadora();
+		System.out.println(miCalc.suma(5,3));
+		System.out.println(miCalc.resta(5,3));
+
 
 	}
 
